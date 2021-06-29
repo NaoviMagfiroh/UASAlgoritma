3bd6afe866924a353e2eb8ce1ba4be4c94a09cfb
NaoviMagfiroh <80589413+NaoviMagfiroh@users.noreply.github.com>
2021-06-29 10:12:46 +0700

    Update AntrianApp.java

--- src/Queue/AntrianApp.java
+++ src/Queue/AntrianApp.java
@@ -2,12 +2,12 @@ package Queue;
 public class AntrianApp {
     public static void main(String args []){
         Antrian antrian = new Antrian (10);
-        antrian.enqueue(3);
+        antrian.enqueue(5);
         antrian.display();
         antrian.enqueue(60);
         antrian.display();
         System.out.println("nilai yang paling depan = " + antrian.peek());
-        System.out.println("Nama saya adalah zulfatun nisa");
+        System.out.println("Nama saya adalah Naovi Magfiroh");
         System.out.println(" ");
         antrian.enqueue(70);
         antrian.display();
